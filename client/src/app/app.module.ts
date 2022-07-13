e1cb51c3ac2f659fbd0a4296a927b1bf7076c2aa
Odhran <mahonyodhran@gmail.com>
2022-07-13 23:56:49 +0100

    added new component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, EmployeesListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, EmployeesListComponent, EmployeeFormComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ client/src/app/employee-form/employee-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-form',
+  template: `
+    <p>
+      employee-form works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class EmployeeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
