edb005e6d5209f62af281830b7e5f12ce6c23a1e
Odhran <mahonyodhran@gmail.com>
2022-07-13 23:24:06 +0100

    get endpoint for specific employee

--- server/src/employee.routes.ts
+++ server/src/employee.routes.ts
@@ -13,3 +13,19 @@ employeeRouter.get('/', async (_req, res) => {
     res.status(500).send(error.message);
   }
 });
+
+employeeRouter.get('/:id', async (req, res) => {
+  try {
+    const id = req?.params?.id;
+    const query = { _id: new mongodb.ObjectId(id) };
+    const employee = await collections.employees.findOne(query);
+
+    if (employee) {
+      res.status(200).send(employee);
+    } else {
+      res.status(404).send(`Failed to find an employee: ID ${id}`);
+    }
+  } catch (error) {
+    res.status(404).send(`Failed to find an employee: ID ${req?.params?.id}`);
+  }
+});
