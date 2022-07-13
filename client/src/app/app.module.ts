eda7c4497015bd875fc4f19854097120429b57fd
Odhran <mahonyodhran@gmail.com>
2022-07-13 23:58:21 +0100

    added add emp component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { EmployeesListComponent } from './employees-list/employees-list.componen
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
 @NgModule({
-  declarations: [AppComponent, EmployeesListComponent, EmployeeFormComponent],
+  declarations: [AppComponent, EmployeesListComponent, EmployeeFormComponent, AddEmployeeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
