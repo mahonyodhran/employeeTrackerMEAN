4ff2d374070b3c101d87671791a2ff3bd8d27be6
Odhran <mahonyodhran@gmail.com>
2022-07-13 23:29:35 +0100

    added delete

--- server/src/employee.routes.ts
+++ server/src/employee.routes.ts
@@ -67,3 +67,21 @@ employeeRouter.put('/:id', async (req, res) => {
     res.status(400).send(error.message);
   }
 });
+
+employeeRouter.delete("/:id", async (req, res) => {
+    try {
+        const id = req?.params?.id;
+        const query = { _id: new mongodb.ObjectId(id) };
+        const result = await collections.employees.deleteOne(query);
+  
+        if (result && result.deletedCount) {
+            res.status(202).send(`Removed an employee: ID ${id}`);
+        } else if (!result) {
+            res.status(400).send(`Failed to remove an employee: ID ${id}`);
+        } else if (!result.deletedCount) {
+            res.status(404).send(`Failed to find an employee: ID ${id}`);
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
